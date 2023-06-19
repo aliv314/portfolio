07e98094ddda2c2c5f84182abe32ff79f55f4620
alv-314 <alicov314@gmail.com>
2023-06-19 11:51:32 -0400

    Custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'dark-blue': '0F052D',
+        'navy-blue': '203671',
+        'teal': '36868F',
+        'lime': '5FC75D',
+      },
+    },
   },
   plugins: [],
 }
