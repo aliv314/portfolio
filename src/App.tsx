3b1d97f2bcdc571ebdbb94edadfde1ae8342c21b
alv-314 <alicov314@gmail.com>
2023-06-21 00:09:20 -0400

    Swapped back to using <a> in <Menu.Item>

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 function App() {
-  // Order is About, Projects, Skills, Blog
+  const aboutRef = useRef(null)
+  const projectsRef = useRef(null)
+  const skillsRef = useRef(null)
+  const blogRef = useRef(null)
+  
+
   return (
     <>
     <Navbar/>
     <main className=' bg-teal py-12 h-full w-full'>
-      <section id="About"> 
+      <section id="about"> 
         About 
         <p>
         Pokem ipsum dolor sit amet Walrein Fraxure Zapdos Rayquaza Starly Klink. Volcano Badge surrender now or prepare to fight Escavalier Shroomish blast off at the speed of light Glaceon Zapdos. Thundershock Braviary Sand-Attack Grotle Pallet Town Scraggy Mienfoo. Rock Graveler Leech Seed Registeel Articuno Chingling Technical Machine. Fire Red Pokemon Vanillish Pichu Maractus Heatran Flaaffy.
         </p>
       </section>
-      <section id="Projects"> 
+      <section id="projects"> 
         Projects 
         <p>
         Pokem ipsum dolor sit amet Walrein Fraxure Zapdos Rayquaza Starly Klink. Volcano Badge surrender now or prepare to fight Escavalier Shroomish blast off at the speed of light Glaceon Zapdos. Thundershock Braviary Sand-Attack Grotle Pallet Town Scraggy Mienfoo. Rock Graveler Leech Seed Registeel Articuno Chingling Technical Machine. Fire Red Pokemon Vanillish Pichu Maractus Heatran Flaaffy.
         </p>
       </section>
-      <section id="Skills"> 
+      <section id="skills"> 
         Skills 
         <p>
         Pokem ipsum dolor sit amet Walrein Fraxure Zapdos Rayquaza Starly Klink. Volcano Badge surrender now or prepare to fight Escavalier Shroomish blast off at the speed of light Glaceon Zapdos. Thundershock Braviary Sand-Attack Grotle Pallet Town Scraggy Mienfoo. Rock Graveler Leech Seed Registeel Articuno Chingling Technical Machine. Fire Red Pokemon Vanillish Pichu Maractus Heatran Flaaffy.
         </p>
       </section>
-      <section id="Blog"> Blog </section>
+      <section id="blog"> 
+        Blog 
+        <p>
+        Pokem ipsum dolor sit amet Walrein Fraxure Zapdos Rayquaza Starly Klink. Volcano Badge surrender now or prepare to fight Escavalier Shroomish blast off at the speed of light Glaceon Zapdos. Thundershock Braviary Sand-Attack Grotle Pallet Town Scraggy Mienfoo. Rock Graveler Leech Seed Registeel Articuno Chingling Technical Machine. Fire Red Pokemon Vanillish Pichu Maractus Heatran Flaaffy.
+        </p>
+      </section>
     </main> 
     </>
   );
