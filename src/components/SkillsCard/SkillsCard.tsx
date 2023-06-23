e016d3d10850de6f882cc04845ee439617923ddd
alv-314 <alicov314@gmail.com>
2023-06-23 10:29:17 -0400

    Skills card looks presentable

--- src/components/SkillsCard/SkillsCard.tsx
+++ src/components/SkillsCard/SkillsCard.tsx
@@ -1,15 +1,22 @@
 import {FC} from 'react'
 
-const skillsPlaceHolder = ["HTML"," CSS"," SCSS"," JavaScript"," Node"," React"," Express"," MySQL"," PostgreSQL"," Agile Development"," Document Object Model (DOM) APIs"," Web APIs"," User Authentication"," OAuth"," Heroku"," Spring Boot"," Firebase"," Firestore"," GitHub"," Jest"," Python"," Java"," C"," Swift"," Xcode"," Android Studio"]
+let skillsPlaceHolder:string[] = ["HTML"," CSS"," SCSS"," JavaScript"," Node"," React"," Express"," MySQL"," PostgreSQL"," Agile Development"," Document Object Model API"," Web APIs"," User Authentication"," OAuth"," Heroku"," Spring Boot"," Firebase"," Firestore"," GitHub"," Jest"," Python"," Java","C"," Swift"," Xcode","Android Studio"]
+skillsPlaceHolder.sort(function(a, b) {
+    return a.length - b.length || // sort by length, if equal then
+           a.localeCompare(b);    // sort by dictionary order
+  });
+
 
 const SkillsCard:FC = () => {
     return(
-    <div className='bg-navy-blue flex flex-wrap rounded-lg p-10'>
+    <div className='bg-navy-blue flex flex-wrap rounded-lg p-8'>
     {skillsPlaceHolder.map(skill => {
         return (
             <ul>
-                <li className='bg-lime rounded-md p-2 m-2 text-dark-blue select-none'>
+                <li className='bg-lime rounded-md p-2 m-2 flex items-center justify-center select-none'>
+                    <p className='text-dark-blue text-center'>
                     {skill}
+                    </p>
                 </li>
             </ul>
         )
