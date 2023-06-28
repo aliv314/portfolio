a349e1bed7c51464526396aee7ed521aa5d7a9fc
alv-314 <alicov314@gmail.com>
2023-06-27 21:19:31 -0400

    Added tooltips to skill section

--- src/components/SkillTag/SkillTag.tsx
+++ src/components/SkillTag/SkillTag.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-
+import SkillsCard from '../SkillsCard/SkillsCard';
 interface TagProp {
     skill: String
 }
@@ -7,8 +7,17 @@ interface TagProp {
 const SkillTag: FC<TagProp> = (props) => {
     const { skill } = props;
 
-    return (<li className='bg-lime text-dark-blue text-center whitespace-nowrap text-ellipsis overflow-auto rounded-md max-w-[100px] h-12 p-2 m-2 flex items-center select-none'>
-        {skill}
+    return (<li className='relative group '>
+        <div className="absolute flex justify-center items-center bottom-[3.5rem] bg-dark-blue h-12 rounded-3xl px-4 py-4 invisible group-hover:visible">
+            <p className='text-white w-full text-center whitespace-nowrap'>
+                {skill}
+            </p>
+        </div>
+        <div className='bg-lime rounded-md max-w-[7rem] h-12 px-2 py-5 m-2 flex items-center justify-start select-none'>
+            <p className='text-dark-blue w-full text-ellipsis overflow-hidden text-center whitespace-nowrap'>
+                {skill}
+            </p>
+        </div>
     </li>)
 }
 
