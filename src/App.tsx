174678e1e884ec30f2bd2f7c419b41e7276d642b
alv-314 <alicov314@gmail.com>
2023-06-30 23:15:03 -0400

    Formatting main card. integrated skills into it

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { useRef } from 'react';
 
-import Navbar from './components/NavBar/Navbar';
 import AboutCard from './components/AboutCard/AboutCard';
-import SkillsCard from './components/SkillsCard/SkillsCard';
-import ProjectCard from './components/ProjectCard/ProjectCard';
+
 
 function App() {
   const aboutRef = useRef(null)
   const projectsRef = useRef(null)
   const skillsRef = useRef(null)
   const blogRef = useRef(null)
+  const contact = useRef (null)
 
   
   return (
@@ -20,19 +19,6 @@ function App() {
       <section ref={aboutRef} className = "mb-8"> 
         <AboutCard></AboutCard>
       </section>
-      <section ref = {projectsRef} className ="mb-8"> 
-        <ProjectCard></ProjectCard>
-      </section>
-      <section ref = {skillsRef} className ="mb-8"> 
-        Skills  
-        <SkillsCard></SkillsCard>
-      </section>
-      {/* <section ref = {blogRef} className ="mb-8"> 
-        Blog 
-        <p>
-        Pokem ipsum dolor sit amet Walrein Fraxure Zapdos Rayquaza Starly Klink. Volcano Badge surrender now or prepare to fight Escavalier Shroomish blast off at the speed of light Glaceon Zapdos. Thundershock Braviary Sand-Attack Grotle Pallet Town Scraggy Mienfoo. Rock Graveler Leech Seed Registeel Articuno Chingling Technical Machine. Fire Red Pokemon Vanillish Pichu Maractus Heatran Flaaffy.
-        </p>
-      </section> */}
     </main> 
     </>
   );
--- src/components/AboutCard/AboutCard.tsx
+++ src/components/AboutCard/AboutCard.tsx
@@ -1,18 +1,38 @@
-import {FC} from 'react'
+import { FC } from 'react'
+import SkillTag from '../SkillTag/SkillTag'
 
-const AboutCard:FC = () => {
+
+const AboutCard: FC = () => {
+    let skillsPlaceHolder: string[] = ["HTML", " CSS", " SCSS", " JavaScript", " Node", " React", " Express", " MySQL", " PostgreSQL", " Agile Development", " Document Object Model API", " Web APIs", " User Authentication", " OAuth", " Heroku", " Spring Boot", " Firebase", " Firestore", " GitHub", " Jest", " Python", " Java", "C", " Swift", " Xcode", "Android Studio"]
+
+    skillsPlaceHolder.sort(function (a, b) {
+        return Math.random() - 0.5  // sort by dictionary order
+    });
+
     const name = `Alejandro Vecchio`
-    const aboutText  = `I’m a recent computer science and software engineering bootcamp graduate with a passion for problem solving and optimizing software solutions. Given my academic background, I’m committed to continuously expanding my knowledge and excel in fast-paced environments with high expectations.`
-      
+    const aboutText = `I’m a recent computer science and software engineering bootcamp graduate with a passion for problem solving and optimizing software solutions. Given my academic background, I’m committed to continuously expanding my knowledge and excel in fast-paced environments with high expectations.`
+
     return (
-        <div className='bg-navy-blue items-center rounded-3xl p-10 '>
-            <div className=''>
-                
+        <div className='bg-navy-blue items-center rounded-3xl p-10 flex flex-wrap flex-col'>
+            <div className='mb-4 mr-auto'>
+                <h1 className='text-xl mb-1'> {name} </h1>
+                <p className='p-2 border-l-2 text-xs'> Full-Stack Software Engineer </p>
             </div>
-            <h1 className='text-xl mb-4'> {name} </h1>
-            <p className='leading-6'> {aboutText} </p>
+            <div className='mb-4'>
+                <p className='text-sm leading-[1.25rem]'> {aboutText} </p>
+            </div>
+            <div className='bg-navy-blue flex flex-wrap rounded-lg mb-4'>
+                {skillsPlaceHolder.map(skill => {
+                    return (
+                        <ul>
+                            <SkillTag skill={skill}></SkillTag>
+                        </ul>
+                    )
+                })}
+            </div>
+
         </div>
-    
+
     )
 }
 
