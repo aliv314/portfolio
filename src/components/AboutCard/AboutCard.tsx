867676f8e196f95a84108fd5364cb1edd2465410
alv-314 <alicov314@gmail.com>
2023-06-30 23:59:02 -0400

    Finished basic styling

--- src/components/AboutCard/AboutCard.tsx
+++ src/components/AboutCard/AboutCard.tsx
@@ -6,23 +6,22 @@ import devPic from '../../assets/images/images/DevPic.jpg'
 const AboutCard: FC = () => {
     let skillsPlaceHolder: string[] = ["HTML", " CSS", " SCSS", " JavaScript", " Node", " React", " Express", " MySQL", " PostgreSQL", " Agile Development", " Document Object Model API", " Web APIs", " User Authentication", " OAuth", " Heroku", " Spring Boot", " Firebase", " Firestore", " GitHub", " Jest", " Python", " Java", "C", " Swift", " Xcode", "Android Studio"]
 
-    skillsPlaceHolder.sort(function (a, b) {
-        return Math.random() - 0.5  // sort by dictionary order
-    });
+    // skillsPlaceHolder.sort(function (a, b) {
+    //     return Math.random() - 0.5  // sort by dictionary order
+    // });
 
-    const name = `Alejandro Vecchio`
     const aboutText = `I’m a recent computer science and software engineering bootcamp graduate with a passion for problem solving and optimizing software solutions. Given my academic background, I’m committed to continuously expanding my knowledge and excel in fast-paced environments with high expectations.`
 
     return (
         <div className='bg-navy-blue items-center rounded-3xl flex flex-wrap flex-col max-w-[480px]'>
             {/* Hero */}
-            <div className='relative mb-8 w-auto min-h-[12rem] bg-hero rounded-3xl bg-cover bg-center flex justify-center items-end'>
-                <img src={devPic} className="relative top-14 rounded-full w-[35%] h-1/4 aspect-square" alt="My headshot"></img>
+            <div className='relative mb-8 w-auto min-h-[12rem] bg-hero rounded-t-3xl bg-cover bg-center flex justify-center items-end'>
+                <img src={devPic} className="relative top-10 rounded-full w-[35%] h-1/4 aspect-square" alt="My headshot"></img>
             </div>
             {/* Bottom */}
-            <div className='p-6'>
+            <div className='px-6 my-4'>
                 <div className='mb-4 mr-auto'>
-                    <h1 className='text-xl mb-1'> {name} </h1>
+                    <h1 className='text-xl mb-1'> Alejandro Vecchio </h1>
                     <p className='p-2 border-l-2 text-xs'> Full-Stack Software Engineer </p>
                 </div>
                 <div className='mb-4'>
