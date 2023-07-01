51323f63809cfb3e0686dae521755f2522187506
alv-314 <alicov314@gmail.com>
2023-07-01 06:58:38 -0400

    fixed rendering bug with tags

--- src/components/AboutCard/AboutCard.tsx
+++ src/components/AboutCard/AboutCard.tsx
@@ -4,18 +4,12 @@ import SkillTag from '../SkillTag/SkillTag'
 import devPic from '../../assets/images/images/DevPic.jpg'
 
 const AboutCard: FC = () => {
-    let skillsPlaceHolder: string[] = ["HTML", " CSS", " SCSS", " JavaScript", " Node", " React", " Express", " MySQL", " PostgreSQL", " Agile Development", " Document Object Model API", " Web APIs", " User Authentication", " OAuth", " Heroku", " Spring Boot", " Firebase", " Firestore", " GitHub", " Jest", " Python", " Java", "C", " Swift", " Xcode", "Android Studio"]
-
-    // skillsPlaceHolder.sort(function (a, b) {
-    //     return Math.random() - 0.5  // sort by dictionary order
-    // });
-
-    const aboutText = `I’m a recent computer science and software engineering bootcamp graduate with a passion for problem solving and optimizing software solutions. Given my academic background, I’m committed to continuously expanding my knowledge and excel in fast-paced environments with high expectations.`
+    let skillsPlaceHolder: string[] = ["HTML", " CSS", " SCSS", " JavaScript", "TypeScript", "Tailwind", " Node", " React", " Express", " MySQL", " PostgreSQL", " Agile Development", " Document Object Model API", " Web APIs", " User Authentication", " OAuth", " Heroku", " Spring Boot", " Firebase", " Firestore", " GitHub", " Jest", " Python", " Java", "C", " Swift", " Xcode", "Android Studio"]
 
     return (
         <div className='bg-navy-blue items-center rounded-3xl flex flex-wrap flex-col max-w-[480px]'>
             {/* Hero */}
-            <div className='relative mb-8 w-auto min-h-[12rem] bg-hero rounded-t-3xl bg-cover bg-center flex justify-center items-end'>
+            <div className='relative mb-8 w-auto min-h-[8rem] bg-hero rounded-t-3xl bg-cover bg-center flex justify-center items-end'>
                 <img src={devPic} className="relative top-10 rounded-full w-[35%] h-1/4 aspect-square" alt="My headshot"></img>
             </div>
             {/* Bottom */}
@@ -24,17 +18,14 @@ const AboutCard: FC = () => {
                     <h1 className='text-xl mb-1'> Alejandro Vecchio </h1>
                     <p className='p-2 border-l-2 text-xs'> Full-Stack Software Engineer </p>
                 </div>
-                <div className='mb-4'>
-                    <p className='text-sm leading-[1.25rem]'> {aboutText} </p>
-                </div>
-                <div className='bg-navy-blue flex flex-wrap rounded-lg mb-4'>
+                <div className='bg-navy-blue rounded-lg mb-4'>
+                    <ul className='flex flex-wrap'>
                     {skillsPlaceHolder.map(skill => {
                         return (
-                            <ul>
                                 <SkillTag skill={skill}></SkillTag>
-                            </ul>
-                        )
-                    })}
+                                )
+                            })}
+                    </ul>
                 </div>
             </div>
 
