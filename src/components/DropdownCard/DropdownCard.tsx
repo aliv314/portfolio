d928d1cb2febca2b686f4f0f15882c34b9860c32
alv-314 <alicov314@gmail.com>
2023-07-02 22:47:18 -0400

    Arrow animation :) omg

--- src/components/DropdownCard/DropdownCard.tsx
+++ src/components/DropdownCard/DropdownCard.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useState } from "react";
 import Arrow from "@mui/icons-material/ArrowForward";
-
 interface DropdownProps {
   title: String;
   toggleOpen: Function;
@@ -8,24 +7,22 @@ interface DropdownProps {
 
 const Dropown: FC<DropdownProps> = (props) => {
   const { title, toggleOpen } = props;
-  const [arrowOpen, setArrow] = useState(false);
-
-  const toggleArrow= () => setArrow((cur) => !cur);
-
+  const [arrowState, setArrow] = useState(false);
+  const toggleArrow = () => setArrow(curr => !curr);
 
   return (
     <>
       <div
         onClick={() => {
           toggleOpen();
-          toggleArrow()
+          toggleArrow();
         }}
         className="bg-navy-blue px-6 rounded-3xl flex items-center justify-center h-[3.5rem] max-w-[480px] mb-2"
       >
         <h2 className="text-lg mr-auto"> {title} </h2>
-        <Arrow className={
-            `${arrowOpen ? "rotate-0" : "rotate-90"}`
-        }></Arrow>
+        <div className={`transition-all duration-500 ${arrowState ? "rotate-0" : " rotate-90"}`}>
+            <Arrow/>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,46 +2,23 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend:{
-      keyframes:{
-        turnside:{
-            'from' : {transform: 'rotate(0)'},
-            'to': { transform: 'rotate(90deg)'}
-        },
-        turnback:{
-          'from': { transform: 'rotate(90deg)'},
-          'to' : {transform: 'rotate(0)'},
-        }
-      },
-      backgroundImage:{
-        'hero': "url('/src/assets/images/images/bg.jpg')",
-      },
-
-    },
     colors: {
       // Remember. When making night mode:
-      // BG color and text color are hard-coded 
+      // BG color and text color are hard-coded
       // Loot at index.css
-      transparent: 'transparent',
-      current: 'currentColor',
-      'dark-blue': '#0F052D',
-      'navy-blue': '#203671',
-      'teal': '#36868F',
-      'lime': '#5FC75D',
+      transparent: "transparent",
+      current: "currentColor",
+      "dark-blue": "#0F052D",
+      "navy-blue": "#203671",
+      teal: "#36868F",
+      lime: "#5FC75D",
     },
-    screens:{
-      'tablet': '768px',
-      'desktop': '1280px',
+    screens: {
+      tablet: "768px",
+      desktop: "1280px",
     },
-    animation: {
-      'spin-slow': 'spin 20s linear infinite',
-      'turnside': 'turnside 0.2s linear',
-      'turnback': 'turnback 0.2s linear'
-    }
   },
   plugins: [],
-})
+});
